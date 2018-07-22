1cd5a7aef35f10da69f783d02632e4e088477d9c
Uros Stojiljkovic <urosss007@gmail.com>
2018-07-22 14:15:17 +0200

    first commit - display and main loop

---
+++ src/pacman/Game.java
@@ -0,0 +1,97 @@
+package pacman;
+
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import pacman.display.Display;
+
+public class Game implements Runnable {
+
+    private Display display;
+    private String title;
+    private int width, height;
+
+    private boolean running = false;
+    private Thread thread;
+
+    private BufferStrategy bs;
+    private Graphics g;
+
+    public Game(String title, int width, int height) {
+        this.title = title;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void init() {
+        display = new Display(title, width, height);
+    }
+
+    public void tick() {
+
+    }
+
+    public void render(Graphics g) {
+        bs = display.getCanvas().getBufferStrategy();
+        if (bs == null) {   // calling this method fot the first time
+            display.getCanvas().createBufferStrategy(3);
+            return;
+        }
+        g = bs.getDrawGraphics();
+
+        // Clear screen
+        g.clearRect(0, 0, width, height);
+
+        // Draw here:
+        // End drawing
+        bs.show();
+        g.dispose();
+    }
+
+    @Override
+    public void run() {
+        init();
+
+        int fps = 60;
+        double timePerTick = 1000000000.0 / fps;
+        double delta = 0;
+        long now, timer = 0;
+        int ticks = 0;
+        long lastTime = System.nanoTime();
+
+        while (running) {
+            now = System.nanoTime();
+            delta += (now - lastTime) / timePerTick;
+            timer += now - lastTime;
+            lastTime = now;
+
+            if (delta >= 1) {
+                tick();
+                render(g);
+                ticks++;
+                delta--;
+            }
+
+            if (timer >= 1000000000) {
+                System.out.println("Ticks and frames: " + ticks);
+                ticks = 0;
+                timer = 0;
+            }
+        }
+
+        stop();
+    }
+
+    public synchronized void start() {
+        if (running) {
+            return;
+        }
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void stop() {
+
+    }
+
+}
---
+++ src/pacman/Launcher.java
@@ -0,0 +1,11 @@
+package pacman;
+
+import pacman.display.Display;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        new Game("Pacman", 500, 500).start();
+    }
+    
+}
